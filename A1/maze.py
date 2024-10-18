5424366319c6752b520baea795bac12750a9cfb6
Ayaan Alam <158285412+ayaandroid@users.noreply.github.com>
2024-10-18 15:26:18 +0530

    Add files via upload

---
+++ A1/maze.py
@@ -0,0 +1,25 @@
+class Maze:
+	def __init__(self, m: int, n : int) -> None:
+        ## DO NOT MODIFY THIS
+        ## We initialise the list with all 0s, as initially all cells are vacant
+		self.grid_representation = []
+		for row in range(m):
+			grid_row = []
+			for column in range(n):
+				grid_row.append(0)
+			self.grid_representation.append(grid_row)
+					
+	def add_ghost(self, x : int, y: int) -> None:
+		self.grid_representation[x][y] = 1
+		
+	def remove_ghost(self, x : int, y: int) -> None:
+		self.grid_representation[x][y] = 0
+		
+	def is_ghost(self, x : int, y: int) -> bool:
+		return self.grid_representation[x][y] == 1
+		
+	def print_grid(self) -> None:
+		for row in self.grid_representation:
+			for cell in row:
+				print(cell, end = ' ')
+			print()
---
+++ A1/navigator.py
@@ -0,0 +1,42 @@
+from maze import *
+from exception import *
+from stack import *
+
+class PacMan:
+	
+	navigator_maze = []
+	
+	def __init__(self, grid):
+		self.navigator_maze = grid.grid_representation
+		
+	def find_path(self, start, end):
+		if self.navigator_maze[start[0]][start[1]] == 1 or self.navigator_maze[end[0]][end[1]] == 1:
+			raise PathNotFoundException
+			
+		m = len(self.navigator_maze)
+		n = len(self.navigator_maze[0])
+		
+		visited = [[False]*n for i in range(m)]
+		
+		path = Stack(max_size = m*n)
+		path.push(start)
+		
+		while not path.is_empty():
+			current = path.get_top()
+			visited[current[0]][current[1]] = True
+			
+			if current == end:
+				return path.get_L()
+				
+			if current[0]+1 < m and not visited[current[0]+1][current[1]] and self.navigator_maze[current[0]+1][current[1]] != 1:
+				path.push((current[0]+1, current[1]))
+			elif current[0]-1 >= 0 and not visited[current[0]-1][current[1]] and self.navigator_maze[current[0]-1][current[1]] != 1:
+				path.push((current[0]-1, current[1]))
+			elif current[1]+1 < n and not visited[current[0]][current[1]+1] and self.navigator_maze[current[0]][current[1]+1] != 1:
+				path.push((current[0], current[1]+1))
+			elif current[1]-1 >= 0 and not visited[current[0]][current[1]-1] and self.navigator_maze[current[0]][current[1]-1] != 1: 
+				path.push((current[0], current[1]-1))
+			else:
+				path.pop()
+			
+		raise PathNotFoundException
