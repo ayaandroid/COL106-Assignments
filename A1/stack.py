c832516ff8ef5f827a81fdcd6b41a01e38bdd726
Ayaan Alam <158285412+ayaandroid@users.noreply.github.com>
2024-11-26 16:36:30 +0530

    Update stack.py

--- A1/stack.py
+++ A1/stack.py
@@ -1,39 +1,15 @@
-from copy import deepcopy
-
 class Stack:
 	
-	def __init__(self, max_size):
-		self.__L = [None]*max_size
-		self.__top = -1
-		self.__cap = max_size
+	def __init__(self): self.data = []
 
-	def is_empty(self):
-		return self.__top == -1
+	def is_empty(self): return len(self.data) == 0
 	
-	class StackOverFlowException(Exception):
-		def __init__(self):
-			super().__init__("Stack is full")
-	
-	def push(self, e):
-		if self.__top == self.__cap - 1:
-			raise StackOverflowException
-		self.__top += 1
-		self.__L[self.__top] = e
-		
-	class EmptyStackException(Exception):
-		def __init__(self):
-			super().__init__("Stack is empty")
+	def push(self, e): self.data.append(e)
 
 	def pop(self):
-		if self.is_empty():
-			raise EmptyStackException
-		self.__L[self.__top] = None
-		self.__top -= 1
+		if self.is_empty(): raise Exception('Stack is empty')
+		return self.data.pop()
 		
-	def get_top(self):
-		if self.is_empty():
-			raise EmptyStackException
-		return deepcopy(self.__L[self.__top])
-	
-	def get_L(self):
-		return deepcopy(self.__L[:self.__top+1])
+	def top(self):
+		if self.is_empty(): raise Exception('Stack is empty')
+		return self.data[-1]
