b8b60cbe06194a576ab1d223eb44cfc9ddc1585d
Ayaan Alam <158285412+ayaandroid@users.noreply.github.com>
2024-11-26 17:37:39 +0530

    Update run.py

--- A1/run.py
+++ A1/run.py
@@ -5,16 +5,20 @@ def test(grid, start, end):
 
     m, n = len(grid), len(grid[0])
     maze = Maze(m, n)
+    
     for i in range(m): 
         for j in range(n): 
             if grid[i][j] == 1: maze.add_ghost(i,j)
+                
     print('Maze')
     maze.print_grid()
     print('-'*70)
 
     PacManInstance = PacMan(maze)
     path = PacManInstance.find_path(start, end)
+    
     for cell in path: grid[cell[0]][cell[1]] = '*'
+        
     print('Path found successfully')
     for row in grid:
         for cell in row: print(cell, end = '')
