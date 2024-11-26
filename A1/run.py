5964f55eb7cc0ce7ed7eb855b57895b7b26f750a
Ayaan Alam <158285412+ayaandroid@users.noreply.github.com>
2024-11-26 17:34:43 +0530

    Add files via upload

---
+++ A1/run.py
@@ -0,0 +1,36 @@
+from maze import *
+from navigator import *
+
+def test(grid, start, end):
+
+    m, n = len(grid), len(grid[0])
+    maze = Maze(m, n)
+    for i in range(m): 
+        for j in range(n): 
+            if grid[i][j] == 1: maze.add_ghost(i,j)
+    print('Maze')
+    maze.print_grid()
+    print('-'*70)
+
+    PacManInstance = PacMan(maze)
+    path = PacManInstance.find_path(start, end)
+    for cell in path: grid[cell[0]][cell[1]] = '*'
+    print('Path found successfully')
+    for row in grid:
+        for cell in row: print(cell, end = '')
+        print()
+    print('-'*70)
+
+if __name__ == '__main__':
+    
+    grid = [
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0]
+    ]
+
+    start = (2, 0)
+    end = (2, 3)
+
+    test(grid, start, end)
